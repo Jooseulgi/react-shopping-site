4397113a36921043269a04dca794a2a4a6686e12
Jooseulgi <iauuai21@gmail.com>
2023-02-19 23:21:53 +0900

    🎨 (admin) : admin 로그인시 로직 추가

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -6,7 +6,7 @@ import {
   signOut,
   onAuthStateChanged,
 } from 'firebase/auth';
-
+import { getDatabase, ref, get } from '@firebase/database';
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -17,6 +17,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
+const database = getDatabase(app);
 
 export async function login() {
   return signInWithPopup(auth, provider)
@@ -33,7 +34,22 @@ export async function logout() {
 }
 
 export function onUserStateChange(callback: any) {
-  onAuthStateChanged(auth, user => {
-    callback(user);
+  onAuthStateChanged(auth, async user => {
+    const updatedUser = user ? await adminUser(user) : null;
+    callback(updatedUser);
   });
+}
+
+async function adminUser(user: any) {
+  //사용자가 어드민 권한을 가지고 있는지 확인 -> true/false
+  return get(ref(database, 'admins')) //
+    .then(snapshot => {
+      if (snapshot.exists()) {
+        const admins = snapshot.val();
+        console.log(admins);
+        const isAdmin = admins.includes(user.uid);
+        return { ...user, isAdmin };
+      }
+      return user;
+    });
 }
